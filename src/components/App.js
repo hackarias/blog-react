7089fda47c687bcb93a1d082b0542001c415dbdb
hackarias <zack.gustavsson@gmail.com>
2019-07-22 22:59:18 +0200

    Add posts functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PostList from "./PostList";
+import UserHeader from "./UserHeader";
 
 class App extends React.Component {
   render() {
     return (
       <div className="segment ui">
         <PostList />
+        <UserHeader />
       </div>
     );
   }
